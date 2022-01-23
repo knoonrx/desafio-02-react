af4f5a2e0c17c6e75aab7b0189ae06cde0bcd23d
Rodrigo Brandão <k-noon@hotmail.com>
2022-01-22 22:58:03 -0300

    Finalizando

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import {Button} from "./Button";
+import {useEffect, useState} from "react";
+import {api} from "../services/api";
+import {GenreResponseProps} from "../models/GenreResponseProps";
+
+interface Props {
+    selectedGenreId: number,
+    handleClickButton: (id: number) => void,
+}
+
+export function SideBar({selectedGenreId, handleClickButton}: Props) {
+    const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+    useEffect(() => {
+        api.get<GenreResponseProps[]>('genres').then(response => {
+            setGenres(response.data);
+        });
+    }, []);
+
+    return (
+        <nav className="sidebar">
+            <span>Watch<p>Me</p></span>
+
+            <div className="buttons-container">
+                {genres.map(genre => (
+                    <Button
+                        key={String(genre.id)}
+                        title={genre.title}
+                        iconName={genre.name}
+                        onClick={() => handleClickButton(genre.id)}
+                        selected={selectedGenreId === genre.id}
+                    />
+                ))}
+            </div>
+        </nav>
+    );
 }
